c14f95f02de7e89299b74cc23eee311e1d283be6
mayaramachado <mayara_b2k@hotmail.com>
2021-10-16 16:04:46 -0300

    Modal do componente Entrar e Cadastro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,28 +11,22 @@ import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delet
 
 
 
-const routes: Routes = [
-
-  {path:"",redirectTo:'entrar',pathMatch:'full'},
-  {
-    path:"entrar",component:EntrarComponent
-  },
-  {
-    path:"cadastrar",component:CadastrarComponent
-  },
-  { path:"inicio",component:InicioComponent
-
-  },{
-      path:"tema",component:TemaComponent
-  },{
-    path:"tema-edit/:id",component:TemaEditComponent
-  },{
-    path:"tema-delete/:id",component:TemaDeleteComponent
-  },{
-    path:"postagem-edit/:id", component: PostagemEditComponent
-  },{
-    path:"postagem-delete/:id", component: PostagemDeleteComponent
-  }
+const routes: Routes = [ //Todo componente que precisar ter acesso tem que referencia-lo aqui . (qui é um arrey de objetos de rotas
+  //Para separar os obejtos dentro do arrey utiliza-se virgula
+// {} utiliza-se para referencia um objeto e dentro dessa chave colocar os atributos dos objetos
+//path: - é nome da minha rota
+//"", nas aspas e virgula é onde vai o nome da minha rota ("entrar")
+// redirectTo: é onde falo q essa rota vai para algum lugar (,component:)
+// : indico qual componete essa rota vai.   (EntrarComponent)
+  { path:"",redirectTo:'entrar',pathMatch:'full'}, //Ele traz todas as rotas a baixo. 
+  { path:"entrar",component:EntrarComponent  },
+  { path:"cadastrar",component:CadastrarComponent  },
+  { path:"inicio",component:InicioComponent  },
+  { path:"tema",component:TemaComponent  },
+  { path:"tema-edit/:id",component:TemaEditComponent  },
+  { path:"tema-delete/:id",component:TemaDeleteComponent  },
+  { path:"postagem-edit/:id", component: PostagemEditComponent  },
+  { path:"postagem-delete/:id", component: PostagemDeleteComponent  }
 ];
 
 @NgModule({
